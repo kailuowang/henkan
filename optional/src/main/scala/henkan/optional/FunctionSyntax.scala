55d1e9c57eba810922d44d4bcfeb574d949b5d5d
Kailuo Wang <kailuo.wang@gmail.com>
2016-11-07 10:19:30 -0500

    added function syntax for optional

---
+++ optional/src/main/scala/henkan/optional/FunctionSyntax.scala
@@ -0,0 +1,34 @@
+package henkan.optional
+
+import cats.arrow.FunctionK
+import cats.{Functor, ApplicativeError}
+import cats.data.NonEmptyList
+import syntax.all._
+
+trait FunctionSyntax {
+  final class autoOptionalPartial[OptionalA, OptionalB] private[optional] {
+    def apply[F[_], A, B](f: A ⇒ F[B])(whenMissing: NonEmptyList[RequiredFieldMissing] ⇒ F[OptionalB])(
+      implicit
+      toOptional: ToOptional[B, OptionalB],
+      validateFromOptional: ValidateFromOptional[OptionalA, A],
+      F: Functor[F]
+    ): OptionalA ⇒ F[OptionalB] = (oa: OptionalA) ⇒ {
+      validate(oa).to[A].fold(
+        whenMissing,
+        a ⇒ F.map(f(a))(from(_).toOptional[OptionalB])
+      )
+    }
+  }
+
+  def autoOptional[OptionalA, OptionalB] = new autoOptionalPartial[OptionalA, OptionalB]
+
+  implicit class toOptionalOps[A, F[_]: Functor, B](f: A ⇒ F[B]) {
+    def toOptional[OptionalA, OptionalB](
+      whenMissing: NonEmptyList[RequiredFieldMissing] ⇒ F[OptionalB]
+    )(
+      implicit
+      toOptional: ToOptional[B, OptionalB],
+      validateFromOptional: ValidateFromOptional[OptionalA, A]
+    ) = autoOptional[OptionalA, OptionalB](f)(whenMissing)
+  }
+}
--- optional/src/main/scala/henkan/optional/package.scala
+++ optional/src/main/scala/henkan/optional/package.scala
@@ -2,8 +2,9 @@ package henkan
 
 package object optional {
   object syntax {
-    object all extends ValidateFromOptionalSyntax with ToOptionalSyntax
+    object all extends ValidateFromOptionalSyntax with ToOptionalSyntax with FunctionSyntax
     object fromOptional extends ValidateFromOptionalSyntax
     object toOptional extends ToOptionalSyntax
+    object function extends FunctionSyntax
   }
 }
