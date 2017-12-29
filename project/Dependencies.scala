0e73b17f40338d9c7621c64f8db7ccc5ddc1a83e
Kai(luo) Wang <kailuo.wang@gmail.com>
2017-12-29 13:55:43 -0500

    udpated cats and scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val specs2 = "3.8.9"
+    val specs2 = "4.0.0"
   }
 
   val shapeless = Seq("com.chuusai" %% "shapeless" % "2.3.2")
-  val cats = Seq("org.typelevel" %% "cats-core" % "1.0.0-RC2")
+  val cats = Seq("org.typelevel" %% "cats-core" % "1.0.0")
 
   val test = Seq(
     "org.specs2" %% "specs2-core" % Versions.specs2 % "test",
@@ -15,8 +15,8 @@ object Dependencies {
   )
 
   val commonSettings = Seq(
-    scalaVersion := "2.12.3",
-    crossScalaVersions := Seq( "2.11.11", scalaVersion.value),
+    scalaVersion := "2.12.4",
+    crossScalaVersions := Seq( "2.11.12", scalaVersion.value),
     resolvers ++= Seq(
       Resolver.sonatypeRepo("releases")
     ),
