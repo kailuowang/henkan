33ee48127b80f58b5f8b10c9145e61196ebffc58
Kai(luo) Wang <kailuo.wang@gmail.com>
2018-10-03 12:16:30 -0400

    updated deps (#48)
    * updated deps
    
    * Update .travis.yml

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   }
 
   val shapeless = Seq("com.chuusai" %% "shapeless" % "2.3.3")
-  val cats = Seq("org.typelevel" %% "cats-core" % "1.0.1")
+  val cats = Seq("org.typelevel" %% "cats-core" % "1.4.0")
 
   val test = Seq(
     "org.specs2" %% "specs2-core" % Versions.specs2 % "test",
@@ -15,12 +15,12 @@ object Dependencies {
   )
 
   val commonSettings = Seq(
-    scalaVersion := "2.12.4",
+    scalaVersion := "2.12.7",
     crossScalaVersions := Seq( "2.11.12", scalaVersion.value),
     resolvers ++= Seq(
       Resolver.sonatypeRepo("releases")
     ),
-    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.4")
+    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.8")
   )
 
   val settings = commonSettings ++ Seq(
